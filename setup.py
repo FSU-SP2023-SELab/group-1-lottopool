e315c97a275611d732bc38b8a6ecf099780e4d3b
Azure-Agst <me@azureagst.dev>
2023-02-01 10:32:18 -0500

    Rename backend, in prep for frontend

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='lotterypool',
+    name='backend',
     version='0.1.0',
     packages=find_packages(),
     include_package_data=True,
