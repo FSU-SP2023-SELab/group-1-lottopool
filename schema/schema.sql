24015e8f99314b31750b72acf9950734ae604d20
Azure-Agst <me@azureagst.dev>
2023-02-14 15:55:49 -0500

    Update schema types, add old db/table deletion, etc.

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,66 +1,83 @@
-CREATE TABLE `pools`(
-    `id` CHAR(36) NOT NULL,
+/*
+ * group-1-lottopool schema.sql
+ * (c) 2023 FSU-SP2023-SELab group1
+ */
+
+/* Drop database if exists */
+
+DROP DATABASE IF EXISTS `lottopool`;
+CREATE DATABASE `lottopool`;
+
+/* Drop tables if they exist */
+
+DROP TABLE IF EXISTS `lottopool`.`pools`;
+DROP TABLE IF EXISTS `lottopool`.`tickets`;
+DROP TABLE IF EXISTS `lottopool`.`users`;
+DROP TABLE IF EXISTS `lottopool`.`paid_in`;
+DROP TABLE IF EXISTS `lottopool`.`paid_out`;
+DROP TABLE IF EXISTS `lottopool`.`agency`;
+
+/* Make tables */
+
+CREATE TABLE `lottopool`.`users`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `auth0_id` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `lottopool`.`agency`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `name` BIGINT NOT NULL,
+    `address` VARCHAR(255) NOT NULL,
+    `phone` VARCHAR(20) NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `lottopool`.`pools`(
+    `id` INT NOT NULL AUTO_INCREMENT,
     `name` VARCHAR(255) NOT NULL,
     `start` DATETIME NOT NULL,
     `end` DATETIME NOT NULL,
     `jackpot` DOUBLE NOT NULL,
     `ticket_price` DOUBLE NOT NULL,
-    `won` TINYINT(1) NOT NULL,
-    `agency_id` CHAR(36) NOT NULL
-);
-ALTER TABLE
-    `pools` ADD PRIMARY KEY `pools_id_primary`(`id`);
-CREATE TABLE `tickets`(
-    `id` CHAR(36) NOT NULL,
-    `pool_id` CHAR(36) NOT NULL,
-    `user_id` CHAR(36) NOT NULL,
+    `won` TINYINT NOT NULL,
+    `agency_id` INT NOT NULL,
+    FOREIGN KEY(`agency_id`) REFERENCES `agency`(`id`),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `lottopool`.`tickets`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `pool_id` INT NOT NULL,
+    `user_id` INT NOT NULL,
     `value` DOUBLE NOT NULL,
     `picture_url` VARCHAR(255) NOT NULL,
-    `bought` TINYINT(1) NOT NULL
-);
-ALTER TABLE
-    `tickets` ADD PRIMARY KEY `tickets_id_primary`(`id`);
-CREATE TABLE `users`(
-    `id` CHAR(36) NOT NULL,
-    `auth0_id` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `users` ADD PRIMARY KEY `users_id_primary`(`id`);
-CREATE TABLE `paid_in`(
-    `id` CHAR(36) NOT NULL,
+    `acquired` TINYINT NOT NULL,
+    FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`),
+    FOREIGN KEY(`user_id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `lottopool`.`paid_in`(
+    `id` INT NOT NULL AUTO_INCREMENT,
     `amount` DOUBLE NOT NULL,
-    `pool_id` CHAR(36) NOT NULL,
-    `agency_id` CHAR(36) NOT NULL
-);
-ALTER TABLE
-    `paid_in` ADD PRIMARY KEY `paid_in_id_primary`(`id`);
-CREATE TABLE `paid_out`(
-    `id` CHAR(36) NOT NULL,
-    `user_id` CHAR(36) NOT NULL,
-    `pool_id` CHAR(36) NOT NULL,
-    `amount` DOUBLE NOT NULL
-);
-ALTER TABLE
-    `paid_out` ADD PRIMARY KEY `paid_out_id_primary`(`id`);
-CREATE TABLE `agency`(
-    `id` CHAR(36) NOT NULL,
-    `name` BIGINT NOT NULL,
-    `address` VARCHAR(255) NOT NULL,
-    `phone` VARCHAR(255) NOT NULL
-);
-ALTER TABLE
-    `agency` ADD PRIMARY KEY `agency_id_primary`(`id`);
-ALTER TABLE
-    `pools` ADD CONSTRAINT `pools_agency_id_foreign` FOREIGN KEY(`agency_id`) REFERENCES `agency`(`id`);
-ALTER TABLE
-    `tickets` ADD CONSTRAINT `tickets_pool_id_foreign` FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`);
-ALTER TABLE
-    `tickets` ADD CONSTRAINT `tickets_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
-ALTER TABLE
-    `paid_in` ADD CONSTRAINT `paid_in_pool_id_foreign` FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`);
-ALTER TABLE
-    `paid_out` ADD CONSTRAINT `paid_out_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
-ALTER TABLE
-    `paid_out` ADD CONSTRAINT `paid_out_pool_id_foreign` FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`);
-ALTER TABLE
-    `paid_in` ADD CONSTRAINT `paid_in_agency_id_foreign` FOREIGN KEY(`agency_id`) REFERENCES `agency`(`id`);
+    `pool_id` INT NOT NULL,
+    `agency_id` INT NOT NULL,
+    FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`),
+    FOREIGN KEY(`agency_id`) REFERENCES `agency`(`id`),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+CREATE TABLE `lottopool`.`paid_out`(
+    `id` INT NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `pool_id` INT NOT NULL,
+    `amount` DOUBLE NOT NULL,
+    FOREIGN KEY(`pool_id`) REFERENCES `pools`(`id`),
+    FOREIGN KEY(`user_id`) REFERENCES `users`(`id`),
+    PRIMARY KEY (`id`)
+) ENGINE=InnoDB CHARSET=utf8 COLLATE=utf8_general_ci;
+
+/* Make views */
+
+/* TODO: Add views for commonly used queries later */
