bddcec14cb0c83542be80fe64390e138b5f8a321
Azure-Agst <me@azureagst.dev>
2023-02-19 21:41:31 -0500

    Clean up backend src init

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,4 +1,6 @@
+import os
 from flask import Flask
+from flask_cors import CORS
 from .middleware import PrefixMiddleware
 
 
@@ -13,13 +15,14 @@ def create_app(test_config=None):
     # Initialize application
     app = Flask(__name__)
 
-    if (os.environ['FLASK_DEBUG'] == '1'):
-      from flask_cors import CORS
-      CORS(app, origins=['http://localhost:3000', 'http://127.0.0.1:3000'])
+    # Init CORS
+    if os.environ["FLASK_DEBUG"] == "1":
+        print("Debug detected, Starting with CORS...")
+        CORS(app, origins=["http://localhost:3000", "http://127.0.0.1:3000"])
 
     # Init WSGI server to serve everything prefixed w/ `/api`
     # It will return 404 errors for anything not in thie subdir
-    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/api')
+    app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/api")
 
     # Handle testing config, if it was passed in
     # We call this second because we want to overwite MONGO_URI
