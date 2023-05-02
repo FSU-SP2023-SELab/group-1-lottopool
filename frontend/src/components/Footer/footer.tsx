3626ad7dc7a881a5802ed9885595f1e3213703d6
Cole Warren <cew19j@my.fsu.edu>
2023-05-02 09:41:01 -0400

    Login requirement for footer Dashboard/Profile

--- frontend/src/components/Footer/footer.tsx
+++ frontend/src/components/Footer/footer.tsx
@@ -1,13 +1,17 @@
-import { Component } from "solid-js";
+import { Component, Show } from "solid-js";
 import { Link } from "@solidjs/router";
+import { useAuth0 } from "@rturnq/solid-auth0";
 
 const Footer: Component = () => {
+  const auth = useAuth0();
   return (
     <footer class="bg-gray-100 py-6">
       <div class="max-w-4xl mx-auto px-4">
         <nav class="flex justify-center gap-12 items-center text-primary font-bold">
-          <Link href="/dashboard">Dashboard</Link>
-          <Link href="/profile">Profile</Link>
+          <Show when={auth?.user()}>
+            <Link href="/dashboard">Dashboard</Link>
+            <Link href="/profile">Profile</Link>
+          </Show>
           <Link href="/how">How It Works</Link>
           <Link href="/about">About Us</Link>
         </nav>
